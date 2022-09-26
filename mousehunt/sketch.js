a9a83d771fd70ffd640a9ca4f663c537a1b62ee7
Banigesh4910151 <4910151@sps-student.sk.ca>
2022-09-26 11:25:10 -0600

    added

--- mousehunt/sketch.js
+++ mousehunt/sketch.js
@@ -14,11 +14,17 @@ let cheeseSize = 100;
 let cheeseSpeed = 5;
 =======
 let y =200; 
+<<<<<<< Updated upstream
 
 >>>>>>> 67e48cb68803312a8e915eba2695ffe29a98f01f
+=======
+let cheeseSize = 100;
+let cheeseSpeed = 5;
+>>>>>>> Stashed changes
 let mouseSize = 100;
 let mouseSpeed = 5;
 let img;
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 let img1;
 =======
@@ -29,15 +35,22 @@ let cheeseSize = 100;
 
 
 >>>>>>> 67e48cb68803312a8e915eba2695ffe29a98f01f
+=======
+let img1;
+>>>>>>> Stashed changes
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   img = loadImage("cheese.png");
+<<<<<<< Updated upstream
 <<<<<<< HEAD
   img1 = loadImage("mouse.png");
 =======
   img2 = loadImage("mouse.png");
 >>>>>>> 67e48cb68803312a8e915eba2695ffe29a98f01f
+=======
+  img1 = loadImage("mouse.png");
+>>>>>>> Stashed changes
 
 }
 
@@ -48,14 +61,20 @@ function draw() {
   handlekeys();
 }
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 function mouse(){
   image(img1, dx, dy, mouseSize, mouseSize);
+=======
+function mousse(){
+  image(img1, x, y, mouseSize, mouseSize);
+>>>>>>> Stashed changes
 }
 
 
 function cheese() {
   image(img, x, y, cheeseSize, cheeseSize);
+<<<<<<< Updated upstream
 =======
 function cheese(){
   image(img, mouseX, mouseY, cheeseSize, cheeseSize);
@@ -65,6 +84,8 @@ function cheese(){
 function mouse() {
   image(img2, x, y, mouseSize, mouseSize);
 >>>>>>> 67e48cb68803312a8e915eba2695ffe29a98f01f
+=======
+>>>>>>> Stashed changes
 }
 
 function handlekeys() {
@@ -81,6 +102,7 @@ function handlekeys() {
     x -= cheeseSpeed;
   }
 }
+<<<<<<< Updated upstream
 
 function handleMouse() {
 <<<<<<< HEAD
@@ -91,3 +113,5 @@ function handleMouse() {
 
 }
 >>>>>>> 67e48cb68803312a8e915eba2695ffe29a98f01f
+=======
+>>>>>>> Stashed changes
