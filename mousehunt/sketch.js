fb68465a6e0424191fc43dbf9c7cfd9af0c404cc
Banigesh4910151 <4910151@sps-student.sk.ca>
2022-10-21 11:53:56 -0600

    updated to proper version

--- mousehunt/sketch.js
+++ mousehunt/sketch.js
@@ -1,46 +1,95 @@
-// Project Title
-// Your Name
-// Date
+// MouseHunt
+// Mohamed Banigesh
+// 10/17/2022
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - added sound to my project when you die
 
-let x = 200;
+
+//set my variables
+let x = 1500;
 let y =200;
 let cheeseSize = 100;
 let mouseSize = 100;
-let mouseSpeed = 30;
+let mouseSpeed = 20;
 let img;
 let img2;
+let hit = false;
+let img3;
+let state = "false";
+let sound;
+let img4;
+let img5;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  img = loadImage("cheese.png");
-  img2 = loadImage("mouse.png");
-
+  img = loadImage("cheese1.png");
+  img2 = loadImage("mouse5.png");
+  img3 = loadImage("gameOver.jpg");
+  img4 = loadImage("rat.jpg");
+  img5 = loadImage("Retry.png");
+  sound = loadSound("end1.mp3");
+  
 }
+
+
 
 function draw() {
-  background("white");
-  cheese();
-  mouse();
-  handlekeys();
+  //when game is not over keep the game going
+  if (state === "false") {
+    start();
+    cheese();
+    mouse();
+    handlekeys();
+  }
+
+  //when game ends change backgorund image and play a song add retry button
+  if (state === "true") {
+    end();
+    sound.playMode("UntilDone");
+    sound.play();
+    startScreen();
+  }
+  
+  //if rect's hits end game
+  hit = collideRectRect(x , y, mouseSize, mouseSize, mouseX, mouseY, cheeseSize, cheeseSize);
+  if (hit) {
+    state = "true";
+  } 
 }
 
-
-
-
+//image of a cheese
 function cheese(){
   image(img, mouseX, mouseY, cheeseSize, cheeseSize);
   imageMode(CENTER);
 
 }
-function mouse() {
-  image(img2, x, y, mouseSize, mouseSize);
+
+//image of game over screen
+function end() {
+  image(img3, windowWidth/2, windowHeight/2, windowWidth, windowHeight); 
 
 }
 
+//image of a mouse
+function mouse() {
+  image(img2, x, y, 240, 240);
+
+}
+
+//image of a retry button
+function retry() {
+  image(img5, 800, 800, 160, 160);
+}
+
+//image of a kitchen
+function start(){
+  image(img4, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
+
+}
+
+//moves the mouse image using wasd keys
 function handlekeys() {
   if (keyIsDown(87)) {
     y -= mouseSpeed;
@@ -55,6 +104,34 @@ function handlekeys() {
     x -= mouseSpeed;
   }
 }
+
+// if your press rectangle it restarts game
+function mousePressed() {
+  if (state === "true" && mouseInsideRect(660, 960, 720, 870)) {
+    state = "false";
+  } 
+}
+
+//draws my rectangle
+function startScreen() {
+  if (mouseInsideRect(660, 960, 720, 870)) {
+    fill("gray");
+  }
+  else {
+    fill("white");
+  }
+  rect(660, 720, 300, 150);
+  fill("white");
+  retry();
+ 
+}
+
+//keeps track if mouse is inside rect
+function mouseInsideRect(left, right, top, bottom) {
+  return mouseX >= left && mouseX <= right && mouseY >= top && mouseY <= bottom;
+}
+
+
 
 
 
