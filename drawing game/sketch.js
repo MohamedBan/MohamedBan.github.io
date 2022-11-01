1b6f5cd1573c9ab3fbfcfa7c030d9ba938270983
Banigesh4910151 <4910151@sps-student.sk.ca>
2022-11-01 11:47:32 -0600

    beginning of drawing game

--- drawing game/sketch.js
+++ drawing game/sketch.js
@@ -1,15 +1,96 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+const ROWS = 40;
+const COLS = 40;
+let grid;
+let cellWidth;
+let cellHeight;
+let autoPlay = false;
+let gosperGun;
+let colorFiLL = "blue";
 
+function preload() {
+  gosperGun = loadJSON("gosper.json");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cellWidth = width/COLS;
+  cellHeight = height/ROWS;
+  grid = createRandom2dArray(COLS, ROWS);
 }
 
 function draw() {
   background(220);
+  displayGrid(grid);
+}
+
+function keyPressed() {
+  if (key === "e") {
+    grid = create2dArray(COLS, ROWS);
+  }
+
+  if (key === "a") {
+    autoPlay = !autoPlay;
+  }
+  if (key === "g") {
+    grid = gosperGun;
+  }
+  if (key === "1") {
+    colorFiLL = "red";
+    
+  }
+}
+
+
+
+function mousePressed() {
+  let xPos = Math.floor(mouseX/cellWidth);
+  let yPos = Math.floor(mouseY/cellHeight);
+
+  if (grid[yPos][xPos] === 0) {
+    grid[yPos][xPos] = 1;
+  }
+  else if (grid[yPos][xPos] === 1) {
+    grid[yPos][xPos] = 0;
+  }
+}
+
+function displayGrid(grid) {
+  for (let y=0; y<ROWS; y++) {
+    for (let x=0; x<COLS; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill(colorFiLL);
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
+}
+
+function create2dArray(COLS, ROWS) {
+  let emptyArray = [];
+  for (let y=0; y<ROWS; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<COLS; x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
+}
+
+function createRandom2dArray(COLS, ROWS) {
+  let emptyArray = [];
+  for (let y=0; y<ROWS; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<COLS; x++) {
+      if (random(100) < 50) {
+        emptyArray[y].push(0);
+      }
+      else {
+        emptyArray[y].push(1);
+      }
+    }
+  }
+  return emptyArray;
 }
