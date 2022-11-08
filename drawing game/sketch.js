b78dfefa6dbfe334d908dd3fe56203ce76165936
Banigesh4910151 <4910151@sps-student.sk.ca>
2022-11-08 12:17:08 -0600

    fixed some problems

--- drawing game/sketch.js
+++ drawing game/sketch.js
@@ -1,12 +1,15 @@
 const ROWS = 20;
 const COLS = 20;
-let grid;
-let grid2;
+let imageGrid;
+let copyGrid;
 let cellWidth;
 let cellHeight;
 let autoPlay = false;
 let family;
 let colorFiLL = "black";
+
+let colorObject = ["white", "red", "orange", "yellow", "green", "blue","purple", "brown", "black"];
+
 
 function preload() {
   family = loadJSON("family.json");
@@ -14,54 +17,33 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cellWidth = width/2/COLS;
+  cellWidth = width*0.45/COLS;
   cellHeight = height/ROWS;
-  grid = create2dArray(COLS, ROWS);
-  grid2 = create2dArray(COLS, ROWS);
+  imageGrid = create2dArray(COLS, ROWS);
+  copyGrid = create2dArray(COLS, ROWS);
   
 
 }
 
 function draw() {
-  background(220);
-  displayGrid1(grid);
-  displayGrid(grid2);
+  background(255);
+  displayGrid1(family);
+  displayGrid(copyGrid);
 }
 
 function keyPressed() {
   if (key === "e") {
-    grid = create2dArray(COLS, ROWS);
+    copyGrid = create2dArray(COLS, ROWS);
   }
 
   if (key === "a") {
     autoPlay = !autoPlay;
   }
   if (key === "f") {
-    grid = family;
+    imageGrid = family;
   }
-  if (key === "1") {
-    colorFiLL = "red";
-  }
-  if (key === "2") {
-    colorFiLL = "orange";
-  }
-  if (key === "3") {
-    colorFiLL = "yellow";
-  }
-  if (key === "4") {
-    colorFiLL = "green";
-  }
-  if (key === "5") {
-    colorFiLL = "blue";
-  }
-  if (key === "6") {
-    colorFiLL = "purple";
-  }
-  if (key === "7") {
-    colorFiLL = "brown";
-  }
-  if (key === "8") {
-    colorFiLL = "black";
+  if (!isNaN(key)) {
+    colorFiLL = colorObject[Number(key)];
   }
   
   
@@ -74,55 +56,34 @@ function mousePressed() {
   let xPos = Math.floor(mouseX/cellWidth);
   let yPos = Math.floor(mouseY/cellHeight);
 
-  if (grid[yPos][xPos] === 0) {
-    if (colorFiLL === "red"){
-      grid[yPos][xPos] = 1;
-    }
-    if (colorFiLL === "orange"){
-      grid[yPos][xPos] = 2;
-    }
-    if (colorFiLL === "yellow"){
-      grid[yPos][xPos] = 3;
-    }
-    if (colorFiLL === "green"){
-      grid[yPos][xPos] = 4;
-    }
-    if (colorFiLL === "blue"){
-      grid[yPos][xPos] = 5;
-    }
-    if (colorFiLL === "purple"){
-      grid[yPos][xPos] = 6;
-    }
-    if (colorFiLL === "brown"){
-      grid[yPos][xPos] = 7;
-    }
-    if (colorFiLL === "black"){
-      grid[yPos][xPos] = 8;
-    }
-
-    
+  if (copyGrid[yPos][xPos] === 0) {
+    copyGrid[yPos][xPos] = colorObject.indexOf(colorFiLL);
   }
-  else if (grid[yPos][xPos] !== 0) {
-    grid[yPos][xPos] = 0;
+
+
+ 
+  
+  else if (copyGrid[yPos][xPos] !== 0) {
+    copyGrid[yPos][xPos] = 0;
     
   }
 }
 
-let colorObject = ["white", "red", "orange", "yellow", "green", "blue","purple", "brown", "black"];
 
-function displayGrid1(grid) {
+function displayGrid1(imageGrid) {
   for (let y=0; y<ROWS; y++) {
     for (let x=0; x<COLS; x++) {
-      fill(colorObject[grid[y][x]]);
+      fill(colorObject[imageGrid[y][x]]);
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
 }
 
-function displayGrid(){
+function displayGrid(copyGrid){
   for (let y=0; y<ROWS; y++) {
     for (let x=0; x<COLS; x++) {
-      rect(x*cellWidth +width/2, y*cellHeight, cellWidth, cellHeight);
+      fill(colorObject[copyGrid[y][x]]);
+      rect(x*cellWidth + width*0.55, y*cellHeight, cellWidth, cellHeight);
 
     }
   }
